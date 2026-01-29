7d51941a498979d6a7cfd25b48ea031265e4b33f
Wallace <will.ragueneau.dev@gmail.com>
2026-01-29 14:22:07 +0100

    📦️ start and poll ccs811

--- main/I2c/i2c.cpp
+++ main/I2c/i2c.cpp
@@ -34,28 +34,139 @@ void I2c::init()
 		vTaskDelay(pdMS_TO_TICKS(2000));
 		esp_restart();
 	}
+
+	if (!ccs811_check_app_valid())
+	{
+		vTaskDelay(pdMS_TO_TICKS(2000));
+		esp_restart();
+	}
+
+	PRINTF_COLOR(COLOR_BLUE, "ccs811 app is valid\n");
+
+	start_app();
+
+	uint8_t meas_mode[2] = { 0x01, 0x10 };
+	i2c_master_transmit(_ccs811_handle, meas_mode, 2, pdMS_TO_TICKS(100));
+
+	vTaskDelay(pdMS_TO_TICKS(1000));
+
+	PRINTF_COLOR(COLOR_BLUE, "Waiting for first DATA_READY...\n");
+	uint8_t status;
+	constexpr uint8_t status_reg = STATUS_REG;
+	do {
+		i2c_master_transmit_receive(_ccs811_handle, &status_reg, 1, &status, 1, pdMS_TO_TICKS(100));
+		PRINTF_COLOR(COLOR_RED, "status register: 0x%02x (DATA_READY = 0x08)\n", status);
+		vTaskDelay(pdMS_TO_TICKS(100));
+	} while (!(status & 0x08));
+	PRINTF_COLOR(COLOR_GREEN, "First measurement ready!\n");
+
+	xTaskCreatePinnedToCore(task_i2c, "i2c_task", 4096, nullptr, 1, nullptr, 1);
 }
 
-bool I2c::read_ccs811(uint16_t& eco2, uint16_t& tvoc)
+bool I2c::read_ccs811(uint16_t *eco2, uint16_t *tvoc)
 {
-	uint8_t reg = 0x02;
-	uint8_t buf[8] = {};
+	if (!eco2 || !tvoc) return false;
 
-	esp_err_t err = i2c_master_transmit_receive(
-		_ccs811_handle,
-		&reg,
-		1,
-		buf,
-		sizeof(buf),
-		pdMS_TO_TICKS(100)
-	);
-
-	if (err != ESP_OK) {
+	uint8_t status_reg = 0x00;
+	uint8_t status = 0;
+	if (i2c_master_transmit_receive(_ccs811_handle, &status_reg, 1, &status, 1, pdMS_TO_TICKS(100)) != ESP_OK)
+	{
+		PRINTF_COLOR(COLOR_RED, "Couldn't read status register!\n");
 		return false;
 	}
 
-	eco2 = (buf[0] << 8) | buf[1];
-	tvoc = (buf[2] << 8) | buf[3];
+	if (!(status & 0x08))
+	{
+		PRINTF_COLOR(COLOR_RED, "status register: 0x%02x (DATA_READY = 0x08)\n", status);
+
+		uint8_t error_reg = 0xe0;
+		uint8_t error_id = 0;
+		i2c_master_transmit_receive(_ccs811_handle, &error_reg, 1, &error_id, 1, pdMS_TO_TICKS(100));
+
+		PRINTF_COLOR(COLOR_RED, "error register: 0x%02x | error id: 0x%02x\n", error_reg, error_id);
+
+		return false;
+	}
+
+	uint8_t reg = 0x02;
+	uint8_t buf[8] = {};
+	if (i2c_master_transmit_receive(_ccs811_handle, &reg, 1, buf, 8, pdMS_TO_TICKS(100)) != ESP_OK)
+	{
+		PRINTF_COLOR(COLOR_RED, "Couldn't read data register!\n");
+		return false;
+	}
+
+	*eco2 = (buf[0] << 8) | buf[1];
+	*tvoc = (buf[2] << 8) | buf[3];
 
 	return true;
+}
+
+
+bool I2c::ccs811_check_app_valid()
+{
+	constexpr uint8_t status_reg = STATUS_REG;
+	uint8_t status = 0;
+	if (i2c_master_transmit_receive(_ccs811_handle, &status_reg, 1, &status, 1, pdMS_TO_TICKS(100)) != ESP_OK)
+	{
+		PRINTF_COLOR(COLOR_RED, "Couldn't read status register!\n");
+		return false;
+	}
+
+	if (!(status & APP_VALID_BIT))
+	{
+		PRINTF_COLOR(COLOR_RED, "app not valid!: 0x%02x (DATA_READY = 0x10)\n", status);
+		return false;
+	}
+
+	return true;
+}
+
+bool I2c::start_app()
+{
+	uint8_t app_start = APP_START_REG;
+	i2c_master_transmit(_ccs811_handle, &app_start, 1, pdMS_TO_TICKS(100));
+
+	vTaskDelay(pdMS_TO_TICKS(100));
+
+	constexpr uint8_t status_reg = STATUS_REG;
+	uint8_t status = 0;
+	if (i2c_master_transmit_receive(_ccs811_handle, &status_reg, 1, &status, 1, pdMS_TO_TICKS(100)) != ESP_OK)
+	{
+		PRINTF_COLOR(COLOR_RED, "Couldn't read status register!\n");
+		return false;
+	}
+
+	if (!(status & FW_MODE_BIT))
+	{
+		PRINTF_COLOR(COLOR_RED, "status FW_MODE is set!: 0x%02x (FW_MODE = 0x40 bit 7)\n", status);
+		return false;
+	}
+
+	return true;
+}
+
+void I2c::task_i2c(void *pvParameters)
+{
+	PRINTF_COLOR(COLOR_BLUE, "Starting i2c task...\n");
+
+	while (true)
+	{
+		PRINTF_COLOR(COLOR_BLACK, "Reading CCS811...\n");
+
+		uint16_t eco2, tvoc = 0;
+
+		if (!read_ccs811(&eco2, &tvoc))
+		{
+			PRINTF_COLOR(COLOR_RED, "Read operation failed!\n");
+		}
+		else
+		{
+			PRINTF_COLOR(COLOR_GREEN, "eCO2: %u ppm | TVOC: %u ppb\n", eco2, tvoc);
+		}
+
+		vTaskDelay(pdMS_TO_TICKS(3000));
+	}
+
+	vTaskDelete(nullptr);
 }
--- main/I2c/i2c.h
+++ main/I2c/i2c.h
@@ -12,15 +12,28 @@
 
 #define CCS811_ADDR 0x5A
 
+#define STATUS_REG 0x00
+#define MEAS_MODE_REG 0x01
+#define ALG_RESULT_REG 0x02
+#define APP_START_REG 0xF4
+
+#define APP_VALID_BIT 0x10
+#define DATA_READY_BIT 0x08
+#define FW_MODE_BIT 0x80
+
 class I2c
 {
 public:
 	static void init();
-	static bool read_ccs811(uint16_t &eco2, uint16_t &tvoc);
+	static bool read_ccs811(uint16_t *eco2, uint16_t *tvoc);
 
 private:
 	static const char *_TAG;
 	static i2c_master_bus_handle_t _bus_handle;
 	static i2c_master_dev_handle_t _ccs811_handle;
+
+	static void task_i2c(void *pvParameters);
+	static bool ccs811_check_app_valid();
+	static bool start_app();
 };
 #endif //CO2_DETECT_I2C_H
